af82c5ca80b2950822eafc359a308f8df7f5b830
adriano-ms <amaciel_sanchez@hotmail.com>
2024-05-22 13:01:16 -0300

    Implemented VO Pattern

--- src/main/java/br/com/adrianoms/controllers/PersonController.java
+++ src/main/java/br/com/adrianoms/controllers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.adrianoms.model.Person;
+import br.com.adrianoms.data.vo.v1.PersonVO;
 import br.com.adrianoms.services.PersonServices;
 
 @RestController
@@ -25,24 +25,24 @@ public class PersonController {
 	private PersonServices service = new PersonServices();
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
+	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 	
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
+	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
-	}
+	}	
 	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
--- src/main/java/br/com/adrianoms/services/PersonServices.java
+++ src/main/java/br/com/adrianoms/services/PersonServices.java
@@ -6,7 +6,9 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.adrianoms.data.vo.v1.PersonVO;
 import br.com.adrianoms.exceptions.ResourceNotFoundException;
+import br.com.adrianoms.mapper.DozerMapper;
 import br.com.adrianoms.model.Person;
 import br.com.adrianoms.repositories.PersonRepository;
 
@@ -18,23 +20,25 @@ public class PersonServices {
 
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-	public Person findById(Long id) {
+	public PersonVO findById(Long id) {
 		logger.info("Finding one person!");
-		return respository.findById(id)
+		var entity = respository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found to this ID!"));
+		return DozerMapper.parseObject(entity, PersonVO.class);
 	}
 
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		logger.info("Finding all people!");
-		return respository.findAll();
+		return DozerMapper.parseListObjects(respository.findAll(), PersonVO.class);
 	}
 
-	public Person create(Person person) {
+	public PersonVO create(PersonVO person) {
 		logger.info("Creating one person!");
-		return respository.save(person);
+		var entity = respository.save(DozerMapper.parseObject(person, Person.class));
+		return DozerMapper.parseObject(entity, PersonVO.class);
 	}
 
-	public Person update(Person person) {
+	public PersonVO update(PersonVO person) {
 		logger.info("Creating one person!");
 		var entity = respository.findById(person.getId())
 				.orElseThrow(() -> new ResourceNotFoundException("No records found to this ID!"));
@@ -42,7 +46,7 @@ public class PersonServices {
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
-		return respository.save(entity);
+		return DozerMapper.parseObject(respository.save(entity), PersonVO.class);
 	}
 
 	public void delete(Long id) {
